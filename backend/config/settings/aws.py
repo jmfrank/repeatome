6bf60ff3091bcd9bb16cef21d1e7a0e7cf0259e6
isamarmoorh <isabelmoorhead@gmail.com>
2025-08-03 11:22:41 -0700

    Fixed packages for AWS ubuntu

--- backend/config/settings/aws.py
+++ backend/config/settings/aws.py
@@ -42,7 +42,7 @@ DATABASES["default"]["ATOMIC_REQUESTS"] = True
 STATICFILES_DIRS = [
     str(ROOT_DIR.parent / "frontend" / "dist"),
     str(ROOT_DIR.parent / "frontend" / "static"),
-    str(os.environ['VIRTUAL_ENV'] / "lib" / "python3.12" / "site-packages" / "django" / "contrib" / "admin" / "static")
+    os.path.join(os.environ['VIRTUAL_ENV'], 'lib', 'python3.13', 'site-packages', 'django', 'contrib', 'admin', 'static')
 ]
 
 # DEBUG
